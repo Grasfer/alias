e295061d7b6159d0b80644694d0ea68ef3696bb1
Grasfer <grasfer@gmail.com>
2026-02-07 11:46:13 +0100

    Create aliases.fish

---
+++ aliases.fish
@@ -0,0 +1,8 @@
+##### Fish Shell #####
+abbr -a fpS 'flatpak search --columns name,application
+abbr -a fpI  'flatpak install -u '
+abbr -a fpU  'flatpak uninstall '
+abbr -a fpUU  'flatpak uninstall --unused'
+abbr -a fpUD  'flatpak uninstall --delete-data '
+abbr -a fpup 'flatpak update --appstream -y; flatpak update -y'
+abbr -a fpL  'flatpak list --columns name,application '
